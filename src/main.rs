79bb2358a22bea0c2567d88314bc9d891a5b2c6a
Ar37-rs <adyaro37@gmail.com>
2022-09-29 19:23:45 +0700

    Udate main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use flowync::{Flower, Handle, IOError, IntoResult};
 use reqwest::Client;
 use tokio::runtime;
 
+const PPP: f32 = 1.25;
+
 // If download progress not shown (unnoticed due to internet connection too fast),
 // try increase REQ_IMAGE_SIZE to 1024, 2048 or between that accordingly, and
 // if setted large than that may cause slow down at `image::from_image_bytes`,
@@ -15,58 +17,74 @@ fn main() {
     let mut options = eframe::NativeOptions::default();
     options.always_on_top = true;
     eframe::run_native(
-        "Download and show an image with eframe/egui",
+        "Eframe + Tokio integration example",
         options,
         Box::new(|ctx| Box::new(EframeTokioApp::new(ctx))),
     );
+}
+
+#[allow(dead_code)]
+enum Message {
+    CountingStar(Vec<f64>),
+    ImageProgress(usize),
+    ImageError,
+    Default,
+}
+
+#[allow(dead_code)]
+enum Container {
+    Data(Vec<u8>),
+    Image(RetainedImage),
+}
+
+type TypedFlower = Flower<Message, Container>;
+type TypedFlowerHandle = Handle<Message, Container>;
+
+struct EframeTokioApp {
+    rt: runtime::Runtime,
+    flower: TypedFlower,
+    init: bool,
+    next_image: bool,
+    btn_label_prev: String,
+    btn_label_next: String,
+    net_image: NetworkImage,
+    error_msg: Message,
 }
 
 #[derive(Default)]
 struct NetworkImage {
     pub image: Option<RetainedImage>,
     pub file_size: usize,
+    pub tmp_file_size: usize,
+    pub show_image_progress: bool,
     pub error: Option<String>,
+    pub seed: usize,
 }
 
 impl NetworkImage {
     fn set_image(&mut self, image: RetainedImage) {
+        self.error.take();
         self.image = Some(image);
-        let _ = self.error.take();
     }
 
-    fn set_error(&mut self, error: impl ToString) {
-        self.error = Some(error.to_string());
+    fn set_error(&mut self, e: impl ToString) {
+        self.error = Some(e.to_string());
     }
-}
 
-#[allow(dead_code)]
-enum Channel {
-    CountingStar(Vec<f64>),
-    ImageProgress(usize),
-}
-
-#[allow(dead_code)]
-enum Finalize {
-    Data(Vec<u8>),
-    Image(RetainedImage),
-}
-
-type TypedFlower = Flower<Channel, Finalize>;
-type TypedFlowerHandle = Handle<Channel, Finalize>;
-
-struct EframeTokioApp {
-    rt: runtime::Runtime,
-    flower: TypedFlower,
-    init: bool,
-    btn_label: String,
-    net_image: NetworkImage,
-    tmp_file_size: usize,
-    seed: usize,
-    show_image_progress: bool,
+    fn repair(&mut self) {
+        // Convert final file size in Bytes to KB.
+        if self.tmp_file_size >= 1000 {
+            self.tmp_file_size /= 1000;
+            self.file_size = self.tmp_file_size;
+        }
+        self.show_image_progress = false;
+        self.tmp_file_size = 0;
+    }
 }
 
 impl EframeTokioApp {
-    fn new(_ctx: &CreationContext) -> Self {
+    fn new(ctx: &CreationContext) -> Self {
+        ctx.egui_ctx.set_pixels_per_point(PPP);
         Self {
             rt: runtime::Builder::new_multi_thread()
                 .enable_all()
@@ -74,11 +92,11 @@ impl EframeTokioApp {
                 .unwrap(),
             flower: TypedFlower::new(1),
             init: true,
-            btn_label: "Cancel?".into(),
+            next_image: true,
+            btn_label_prev: "Fetch prev image".into(),
+            btn_label_next: "Fetch next image".into(),
             net_image: Default::default(),
-            tmp_file_size: 0,
-            seed: 1,
-            show_image_progress: true,
+            error_msg: Message::Default,
         }
     }
 
@@ -90,7 +108,7 @@ impl EframeTokioApp {
         init
     }
 
-    async fn fetch_image(url: String, handle: &TypedFlowerHandle) -> Result<Finalize, IOError> {
+    async fn fetch_image(url: String, handle: &TypedFlowerHandle) -> Result<Container, IOError> {
         // Build a client
         let client = Client::builder()
             // Needed to set UA to get image file, otherwise reqwest error 403
@@ -107,7 +125,7 @@ impl EframeTokioApp {
 
         if content_type.contains("image/jpeg") || content_type.contains("image/png") {
             let url = response.url().to_string();
-            let cancelation_msg = "Fetching other image canceled.";
+            let cancelation_msg = "Fetching image canceled.";
             let vec_u8 = {
                 let mut vec_u8 = Vec::new();
                 while let Some(a_chunk) = response.chunk().await? {
@@ -117,7 +135,7 @@ impl EframeTokioApp {
                     }
 
                     // Send chunk size as download progress
-                    let progress = Channel::ImageProgress(a_chunk.len());
+                    let progress = Message::ImageProgress(a_chunk.len());
                     handle.send_async(progress).await;
                     a_chunk.into_iter().for_each(|x| {
                         vec_u8.push(x);
@@ -133,22 +151,48 @@ impl EframeTokioApp {
                 return Err(cancelation_msg.into());
             }
 
-            let finalize = Finalize::Image(retained_image);
+            let finalize = Container::Image(retained_image);
             Ok(finalize)
         } else {
             Err(format!("Expected  image/jpeg png, found {}", content_type).into())
         }
     }
 
-    fn spawn_fetch_image(&self, url: String) {
+    fn spawn_fetch_image(&mut self, url: String) {
+        // Set error to None
+        self.net_image.error.take();
+        // Show download image progress
+        self.net_image.show_image_progress = true;
+        // Get flower handle
         let handle = self.flower.handle();
         self.rt.spawn(async move {
             // Don't forget to activate flower here
             handle.activate();
-            let result = Self::fetch_image(url, &handle).await;
-            // And set result
-            handle.set_result(result);
+            let fetch_image = Self::fetch_image(url, &handle).await;
+            // Check if result is error
+            if fetch_image.is_err() {
+                // Blocking for a while here, it's fine because we are going to set the result ASAP anyway.
+                handle.send(Message::ImageError);
+            }
+            // Set result
+            handle.set_result(fetch_image);
         });
+    }
+
+    fn reset_fetch_image(&mut self) {
+        self.net_image.repair();
+        if self.next_image && self.flower.is_canceled() {
+            if self.net_image.seed > 1 {
+                self.net_image.seed -= 1;
+            }
+            self.btn_label_next = "Retry next image?".into();
+        } else if !self.next_image && self.flower.is_canceled() {
+            self.net_image.seed += 1;
+            self.btn_label_prev = "Retry prev image?".into();
+        } else {
+            self.btn_label_next = "Fetch next image".into();
+            self.btn_label_prev = "Fetch prev image".into();
+        }
     }
 }
 
@@ -157,63 +201,102 @@ impl eframe::App for EframeTokioApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             if self.show_init() {
                 // Fetch image
+                self.net_image.seed = 1;
                 let url = format!("https://picsum.photos/seed/1/{}", REQ_IMAGE_SIZE);
                 self.spawn_fetch_image(url);
-                self.show_image_progress = true;
             }
 
             if self.flower.is_active() {
+                let mut fetch_image_finalized = false;
                 self.flower
-                    .extract(|channel| {
-                        // Get Channel::ImageProgress since we only want usize value in this case.
-                        if let Channel::ImageProgress(b) = channel {
-                            self.tmp_file_size += b;
+                    .extract(|message| {
+                        if let Message::ImageProgress(b) = message {
+                            self.net_image.tmp_file_size += b;
+                        } else if let Message::CountingStar(_) = message {
+                            // Do stuff here if message
+                        } else {
+                            // Set the error message if any.
+                            self.error_msg = message;
                         }
                     })
                     .finalize(|result| {
                         match result {
-                            Ok(file_type) => {
-                                // Get FileType::Image since we only want retained image in this case.
-                                if let Finalize::Image(retained_image) = file_type {
-                                    // Convert final file size in Bytes to KB.
-                                    self.tmp_file_size /= 1000;
+                            Ok(container) => {
+                                // Get Container::Image since we only want retained image in this case.
+                                if let Container::Image(retained_image) = container {
                                     self.net_image.set_image(retained_image);
-                                    self.net_image.file_size = self.tmp_file_size;
+                                    fetch_image_finalized = true;
                                 }
                             }
-                            Err(err_msg) => self.net_image.set_error(err_msg),
+                            Err(err) => {
+                                // Get specific error message.
+                                if let Message::ImageError = self.error_msg {
+                                    self.net_image.set_error(err);
+                                    fetch_image_finalized = true;
+                                }
+                            }
                         }
-                        
-                        // Reset value if finalized
-                        self.btn_label = "Refetch other image?".into();
-                        self.tmp_file_size = 0;
-                        self.show_image_progress = false;
-                    });
-            }
 
-            if ui.button(&self.btn_label).clicked() {
-                if self.flower.is_active() {
-                    self.flower.cancel();
-                } else {
-                    // Set error to None
-                    self.net_image.error.take();
-                    // Refetch other image
-                    self.seed += 1;
-                    self.btn_label = "Cancel?".into();
-                    let url = format!(
-                        "https://picsum.photos/seed/{}/{}",
-                        self.seed, REQ_IMAGE_SIZE
-                    );
-                    self.spawn_fetch_image(url);
-                    self.show_image_progress = true;
+                        // Set error message to default here.
+                        self.error_msg = Message::Default;
+                    });
+
+                if fetch_image_finalized {
+                    self.reset_fetch_image();
                 }
             }
 
-            if self.show_image_progress {
+            ui.horizontal(|ui| {
+                if ui.button(&self.btn_label_prev).clicked() {
+                    if self.flower.is_active() {
+                        if self.next_image {
+                            self.btn_label_prev = "Wait we are still fetching...".into();
+                        } else {
+                            self.flower.cancel();
+                        }
+                    } else {
+                        // Refetch prev image
+                        if self.net_image.seed > 1 {
+                            self.net_image.seed -= 1;
+                            let url = format!(
+                                "https://picsum.photos/seed/{}/{}",
+                                self.net_image.seed, REQ_IMAGE_SIZE
+                            );
+                            self.spawn_fetch_image(url);
+                            self.next_image = false;
+                            self.btn_label_prev = "Cancel?".into();
+                        } else {
+                            self.btn_label_prev = "Prev image not available".into();
+                        }
+                    }
+                }
+
+                if ui.button(&self.btn_label_next).clicked() {
+                    if self.flower.is_active() {
+                        if !self.next_image {
+                            self.btn_label_next = "Wait we are still fetching...".into();
+                        } else {
+                            self.flower.cancel();
+                        }
+                    } else {
+                        // Refetch next image
+                        self.net_image.seed += 1;
+                        let url = format!(
+                            "https://picsum.photos/seed/{}/{}",
+                            self.net_image.seed, REQ_IMAGE_SIZE
+                        );
+                        self.spawn_fetch_image(url);
+                        self.next_image = true;
+                        self.btn_label_next = "Cancel?".into();
+                    }
+                }
+            });
+
+            if self.net_image.show_image_progress {
                 ui.horizontal(|ui| {
                     // We don't need to call repaint since we are using spinner here.
                     ui.spinner();
-                    let mut downloaded_size = self.tmp_file_size;
+                    let mut downloaded_size = self.net_image.tmp_file_size;
                     if downloaded_size > 0 {
                         // Convert current file size in Bytes to KB.
                         downloaded_size /= 1000;
@@ -223,15 +306,19 @@ impl eframe::App for EframeTokioApp {
                 });
             }
 
-            if let Some(err_msg) = &self.net_image.error {
-                ui.colored_label(ui.visuals().error_fg_color, err_msg);
+            if let Some(err) = &self.net_image.error {
+                ui.colored_label(ui.visuals().error_fg_color, err);
             }
 
             if let Some(image) = &self.net_image.image {
                 let file_size = self.net_image.file_size;
-                ui.label(format!("File size: {} KB", file_size));
-                ui.label(format!("Image size: {}x{} ", image.width(), image.height()));
-                ui.label("Image URL:");
+                ui.label(format!("Current file size: {} KB", file_size));
+                ui.label(format!(
+                    "Current image size: {}x{} ",
+                    image.width(),
+                    image.height()
+                ));
+                ui.label("Current image URL:");
                 let mut text = image.debug_name();
                 let text_edit = egui::TextEdit::singleline(&mut text).desired_width(1000.0);
                 ui.add(text_edit);
@@ -239,7 +326,7 @@ impl eframe::App for EframeTokioApp {
                 egui::ScrollArea::both()
                     .auto_shrink([true, true])
                     .show(ui, |ui| {
-                        image.show_max_size(ui, image.size_vec2());
+                        image.show_max_size(ui, image.size_vec2() / PPP);
                     });
             }
         });
